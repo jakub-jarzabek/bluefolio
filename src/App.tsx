e17e516dc38cba29ec5c643abac46a2ba9b5e64e
Jakub Jarzabek <kubitssu@gmail.com>
2022-09-10 18:59:49 +0200

    add login func

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Navbar } from './components'
 import { Home } from './screens'
 import Admin from './screens/admin'
